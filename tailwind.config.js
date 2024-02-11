6d0f1d3195549477a683bc200808360c1aa3ff5f
Martha Chamberlain <marthachamberlain444@gmail.com>
2024-02-11 07:44:47 -0500

    Add component library

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,23 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/tw-elements-react/dist/js/**/*.js",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        btn: {
-          background: "hsl(var(--btn-background))",
-          "background-hover": "hsl(var(--btn-background-hover))",
-        },
-      },
+      // colors: {
+      //   background: "hsl(var(--background))",
+      //   foreground: "hsl(var(--foreground))",
+      //   btn: {
+      //     background: "hsl(var(--btn-background))",
+      //     "background-hover": "hsl(var(--btn-background-hover))",
+      //   },
+      // },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [require("tw-elements-react/dist/plugin.cjs")],
+  // plugins: [],
 };
