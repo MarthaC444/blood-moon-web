49584671d867e2763a4e87f81a58b6459813b6f9
Martha Chamberlain <marthachamberlain444@gmail.com>
2024-03-05 10:34:27 -0500

    Remove unwanted default landing page code

--- app/page.tsx
+++ app/page.tsx
@@ -2,32 +2,33 @@ import DeployButton from "../components/DeployButton";
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/SignUpUserSteps";
+// import SignUpUserSteps from "@/components/SignUpUserSteps";
 import Header from "@/components/Header";
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
 
 export default async function Index() {
-  const cookieStore = cookies();
+  // const cookieStore = cookies();
 
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient(cookieStore);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
+  // const canInitSupabaseClient = () => {
+  //   // This function is just for the interactive tutorial.
+  //   // Feel free to remove it once you have Supabase connected.
+  //   try {
+  //     createClient(cookieStore);
+  //     return true;
+  //   } catch (e) {
+  //     return false;
+  //   }
+  // };
 
-  const isSupabaseConnected = canInitSupabaseClient();
+  // const isSupabaseConnected = canInitSupabaseClient();
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
+          {/* {isSupabaseConnected && <AuthButton />} */}
+          <AuthButton />
         </div>
       </nav>
 
@@ -35,7 +36,7 @@ export default async function Index() {
         <Header />
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+          <ConnectSupabaseSteps />
         </main>
       </div>
 
